2db3f57c4e042f3ae1a1229a37560c5bff8fc20d
ASHUTOSH SINGH <38chauhan@gmail.com>
2025-07-20 12:25:03 +0530

    add rg1 & provider

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rgroup1name" {
+  name     = "ashurg1"
+  location = "East US"
+}
---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.12.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.33.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "4a8882d8-040a-43b9-81c8-844e324752c0"
+}
