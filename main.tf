6398f3689d90c195c537646cf998cc50657036fe
ASHUTOSH SINGH <38chauhan@gmail.com>
2025-07-22 23:51:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rgroup1name" {
   name     = "ashurg1"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "rgroup2name" {
+  name     = "ashurg2"
+  location = "East US"
+}
