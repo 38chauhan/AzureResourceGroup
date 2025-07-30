22f3a5cd39a032e8b391c5feb22ee38540a5fa5b
ASHUTOSH SINGH <38chauhan@gmail.com>
2025-07-30 22:44:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "rgroup3name" {
   name     = "ashu-rg-03"
   location = "West US"
 }
+
+resource "azurerm_resource_group" "rgroup3name11" {
+  name     = "ashu-rg-004"
+  location = "West US"
+}
+
+resource "azurerm_resource_group" "rgroup3name12" {
+  name     = "ashu-rg-005"
+  location = "West US"
+}
