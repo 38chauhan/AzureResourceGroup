30835f5c0c1ac4747cc60c1d0e97a649f548d867
ASHUTOSH SINGH <38chauhan@gmail.com>
2025-07-30 22:28:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,21 +1,20 @@
 terraform {
- 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.33.0"
+      version = ">= 4.33.0"
     }
-backend "azurerm" {
-resource_group_name = "ashurg-3"       
-storage_account_name = "ashurgstg3"
-container_name = "ashucont3"                             
-key = "prod.terraform.tfstate"               
   }
-}
+
+  backend "azurerm" {
+    resource_group_name   = "ashurg-3"
+    storage_account_name  = "ashurgstg3"
+    container_name        = "ashucont3"
+    key                   = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "16a05a8e-7ba4-486d-8521-57e717aedbbd"
-
 }
